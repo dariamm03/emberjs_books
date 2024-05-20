b6daacb2eceb56862459066538e39e51d91395c6
dariamm03 <osa.m@mail.ru>
2024-05-21 00:31:00 +0500

    ember

---
+++ emberbook/app/router.js
@@ -0,0 +1,15 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('meetings');
+  this.route('speakers');
+  this.route('books');
+});
+
+export default Router;
