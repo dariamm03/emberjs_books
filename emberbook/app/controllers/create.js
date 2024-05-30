c5371e23e2779eba0140f3f0a702cbabda0b26d1
dariamm03 <osa.m@mail.ru>
2024-05-30 15:27:02 +0500

    Update create.js

--- emberbook/app/controllers/create.js
+++ emberbook/app/controllers/create.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import {inject as service} from '@ember/services';
+import {inject as service} from '@ember/service';
 
 export default Controller.extend({
     dataService: service('data'),
